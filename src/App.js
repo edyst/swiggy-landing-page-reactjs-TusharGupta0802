1d642442b570974e2774950fbc97ac2b78f76eb3
TusharGupta0802 <80515032+TusharGupta0802@users.noreply.github.com>
2021-12-17 21:56:41 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import ic from './ic.ico';
 import './App.css';
+import Header from './components/Header';
 
-import {Home} from './components/Home';
-import {Advn} from './components/Advn';
-import {Store} from './components/Store';
-import {About} from './components/About';
-import {Footercity} from './components/Footercity';
-import {Social} from './components/Social';
+import { useState } from 'react';
+import FeatureSection from './components/FeatureSection';
+import PocketSection from './components/PocketSection';
+import CitySection from './components/CitySection.js';
+import ActionSection from './components/ActionSection';
+import Footer from './components/Footer';
+import Slider from './components/Slider';
+import SignIn from './components/SignIn';
+import Login from './components/Login';
+// import LogIn from './components/LogIn';
 
-function App() {
-  return (
-    <div className="container">
-      <Home/>
-      <Advn/>
-      <Store/>
-      <About/>
-      <Footercity/>
-      <Social/>
-    </div>
-  );
+function App (){
+
+   
+
+ 
+        return (
+            <div className="App">
+            <div className="header123"><Header/></div>
+           
+<div className="brightlow">
+            <div className="feature123"><FeatureSection/></div>
+            <div className="pocket123"> <PocketSection/></div>
+            <div className="action123"> <ActionSection/></div>
+            
+            <div className="city123"><CitySection/></div>
+            <div className="footer123"><Footer/></div>
+            </div></div>
+        
+    );
 }
 
 export default App;
